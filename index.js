2359aa27e61d32d0d410c9125403f14c2cf33b1c
Joel Corey <joelcorey@fastmail.com>
2019-02-19 12:35:49 -0800

    beyond function added

--- index.js
+++ index.js
@@ -4,4 +4,21 @@ function jediName(firstName, lastName) {
   return lastName.substring(0,3) + firstName.substring(0,2);
 }
 
-console.log(jediName('Beyonce', 'Knowles'));
+function beyond(num){
+  if(!isFinite(num)){
+    console.log('And beyond!');
+  }
+  if(isFinite(num) && num > 0){
+    console.log('To infinity!');
+  }
+  if(isFinite(num) && num < 0){
+    console.log('To negative infinity!');
+  }
+  if(num === 0){
+    console.log('Staying home!');
+  }
+}
+//console.log(jediName('Beyonce', 'Knowles'));
+
+
+beyond(Number.NEGATIVE_INFINITY);
