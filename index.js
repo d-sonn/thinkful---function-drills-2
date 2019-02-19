56fd9e11811383fc90f917f1b323f8e420edc893
Donald Sonn <dsonn@Donalds-MBP.fios-router.home>
2019-02-19 16:00:14 -0500

    added decode function

--- index.js
+++ index.js
@@ -18,7 +18,33 @@ function beyond(num){
     console.log('Staying home!');
   }
 }
+
+function decode(encodedWord) {
+    switch(encodedWord.substring(0,1)) {
+    case 'a':
+      return encodedWord.substring(1,2);
+    case 'b':
+      return encodedWord.substring(2,3);
+    case 'c':
+      return encodedWord.substring(3,4);
+    case 'd':
+      return encodedWord.substring(4,5);
+    default:
+      return ' '
+  }
+}
+
+let testString = 'craft block argon meter bells brown croon droop';
+let testArray = testString.split(' ');
+let answer = '';
+
+for (var i = 0; i < testArray.length; i++) {
+  answer += decode(testArray[i]);
+}
+
+console.log(answer);
+
 //console.log(jediName('Beyonce', 'Knowles'));
 
+// beyond(Number.NEGATIVE_INFINITY);
 
-beyond(Number.NEGATIVE_INFINITY);
